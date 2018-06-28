acdd7e3eb8bba2ce2d94cc946eafc101d5e43d49
pupoSDC <pupo163@hotmail.com>
2018-06-28 10:04:33 +0200

    Created sum functions

--- example1.cpp
+++ example1.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 
+
+int sum( int a, int b ){
+	return a + b;
+}
+
+int sum( int * a, int * b ){
+	return *a + *b;
+}
+
+int sum( int a, int b ){
+	return a + b;
+}
+
+
 int main() {
 
 	int   a = 124124; // Declaring a variable by Value
 	int * b = &a      // Declaring a pointer to a variable
 	int & c = a;      // Declaring a refernece to a variable
 
-	c = b;
-	//a  = 5;
-	//*b = 7;
 
     std::cout << "Hello, World, I'm " << a << "!" << std::endl;
     return 0;
