a141c943ff429a1a304d448caf53a58e7dd6bb89
pupoSDC <pupo163@hotmail.com>
2018-06-29 10:04:47 +0200

    Created Vehicle header

--- Factory/Vehicle.h
+++ Factory/Vehicle.h
@@ -6,6 +6,17 @@ class Vehicle {
 
 	private:
 
+		std::string vehicle_type;
+		int wheels;
+		int doors;
+		double velocity;
+
 	public:
+
+		Vehicle( const std::string & vehicle_type );
+
+		const std::string & getVehicleType() const;
+
+		double getVelocity() const;
 
 };
--- Factory/factory.cpp
+++ Factory/factory.cpp
@@ -1,6 +1,13 @@
 #include "Vehicle.h"
 #include <iostream>
 
+void makeATrip( const Vehicle & vehicle, double distance ){
+	double time = distance / vehicle.getVelocity();
+	std::cout << "It took the " << vehicle.getVehicleType() 
+			  << " " << time << " to get to the destination!"
+			  << std::endl; 
+}
+
 int main(){
 	
 	std::cout << "Welcome to the factory! " << std::endl;
